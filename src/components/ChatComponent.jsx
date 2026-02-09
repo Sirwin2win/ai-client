8b765387f614461c6be23312a14e19b162ea22da
Sirwin2win <sirwin2win@gmail.com>
2026-02-09 09:33:52 +0100

    changes made

--- src/components/ChatComponent.jsx
+++ src/components/ChatComponent.jsx
@@ -6,20 +6,20 @@ import { useDispatch, useSelector } from 'react-redux';
 
 
 const ChatComponent = () => {
-  const [data, setData] = useState('')
+  const [prompt, setPrompt] = useState('')
   const {chats,status,error} = useSelector(state=>state.chats)
   const dispatch = useDispatch()
   const handleSubmit = (e)=>{
     e.preventDefault()
-      dispatch(createChat(data))
-      console.log(data)
+      dispatch(createChat(prompt))
+      console.log(prompt)
   }
   return (
     <div>
         <form method='post' onSubmit={handleSubmit}>
           {status==='failed'&& <p className='text-danger'>{error}</p>}
             <div className='container' style={{position:"relative"}}>
-                <input type="text" value={data} onChange={(e)=>setData(e.target.value)} name='prompt' className='form-control mx-5' style={{height:'50px',}}  />
+                <input type="text" value={data} onChange={(e)=>setPrompt(e.target.value)} name='prompt' className='form-control mx-5' style={{height:'50px',}}  />
                 <IoMdSend size={50} onSubmit={handleSubmit} className='text-primary' style={{position:'absolute', bottom:0,right:-20}} />
 
             </div>
