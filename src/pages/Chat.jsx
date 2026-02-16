c31d8b062f9dd779e06c74799b648b540032b452
Sirwin2win <sirwin2win@gmail.com>
2026-02-16 10:47:30 +0100

    changes made

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import ChatComponent from '../components/ChatComponent'
+import React, {useState,useEffect} from 'react'
+import { IoMdSend } from "react-icons/io";
+import { createChat, getChat } from '../features/chat/chatSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Chat = () => {
+ const [prompt, setPrompt] = useState('')
+  const {chats,status,error} = useSelector(state=>state.chats)
+  const dispatch = useDispatch()
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+      dispatch(createChat(prompt))
+      console.log(prompt)
+  }
+
+  console.log(chats)
+
+  // useEffect(()=>{
+  //   if(status==='idle'){
+  //     dispatch(getChat())
+  //   }
+  // },[])
   return (
     <div>
-        <h1>Hello!, I am your AI agent, ask me anything</h1>
-        <ChatComponent />
+      <h5 className='my-5 text-primary'>Hello!, I am your AI agent, ask me anything</h5>
+      {chats.map(chat=>(
+        <div className='row'>
+          <p className='col-sm-6'>{chat.role}</p>
+          <p className='col-sm-6'>{chat.parts}</p>
+
+        </div>
+      ))}
+        <form method='post' onSubmit={handleSubmit}>
+          {status==='failed'&& <p className='text-danger'>{error}</p>}
+            <div className='container' style={{position:"relative"}}>
+                <input type="text" value={prompt} onChange={(e)=>setPrompt(e.target.value)} name='prompt' className='form-control mx-5' style={{height:'50px',}}  />
+                <IoMdSend size={50} onClick={handleSubmit} className='text-primary' style={{position:'absolute', bottom:0,right:-20}} />
+
+            </div>
+        </form>
     </div>
   )
 }
